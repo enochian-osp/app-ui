e515ecf52206c05434f5a731af5a327ca8a573c0
Gadi Cohen <dragon@wastelands.net>
2018-10-12 20:08:29 +0200

    dictionary: more super early work

--- src/enochian/dictionary/Dictionary.js
+++ src/enochian/dictionary/Dictionary.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
+import AppBar from '../../AppBar';
+
+import '../enochianFont.js';
+import dictionary from '../enochian-data/dictionary.json';
+
 class DictionaryController extends React.Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      text: ''
+    }
+  }
+
+  onType = event => {
+    const text = event.target.value.toUpperCase();
+    const match = dictionary[text];
+    this.setState({ text, match })
+  }
+
   render() {
-    return ( <div>hi</div> );
+    return (
+      <div>
+        <AppBar title="Dictionary" />
+        <div>
+          <input type="text" value={this.state.text} onChange={this.onType} />
+          <input type="text" className="enochianFont" value={this.state.text} />
+          {this.state.match && JSON.stringify(this.state.match)}
+        </div>
+      </div>
+
+    );
   }
 
 }
